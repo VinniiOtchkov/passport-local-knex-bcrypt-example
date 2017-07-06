b8d9217023eb93280b8fd0cfefd2e11bdea629eb
vinniiotchkov <vinniiotchkov@gmail.com>
2017-07-06 11:31:31 -0700

    first commit

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var knex = require('../db/knex');
 var bcrypt = require('bcrypt');
 
+//Dummy username and password data
 var username = 'blah';
 var password = 'blah';
 
+//Function that checks if the user is authenticated, and if they are, then go to the next piece of middleware. If not, redirect to home directory.
 function checkAuthed(req, res, next) {
   if (req.isAuthenticated()) {
     next();
@@ -19,10 +21,12 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+//Renders the signup view when they go to the extension /signup
 router.get('/signup', function(req, res) {
   res.render('signup');
 });
 
+//Creates post for signup that takes all inputted information and hashes the password
 router.post('/signup', function(req, res) {
   bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(req.body.password, salt, function(err, hashedPassword) {
@@ -37,6 +41,7 @@ router.post('/signup', function(req, res) {
   });
 });
 
+//Gets the users page and checks for authorization and renders the user information as the variable user
 router.get('/user', checkAuthed, function(req, res) {
   res.render('user', {
     user: req.user
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8300;
 var cors = require('cors');
 var logger = require('morgan');
 var knex = require('./db/knex');
+
+//Add these five packages
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -12,7 +14,6 @@ var session = require('express-session');
 var bcrypt = require('bcrypt');
 
 var index = require('./routes/indexRoutes');
-var todos = require('./routes/todosRoutes');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+//Let the server use these packages
 app.use(cookieParser());
 app.use(session({
   secret: 'keyboard cat'
@@ -35,16 +38,22 @@ app.use(passport.session());
 app.use('/', index);
 app.use('/todos', todos);
 
+
+//Create a new instance of Local Strategy(Predefined username and password checks)
 passport.use(new LocalStrategy(
   function(username, password, done) {
     knex('users').select().where('email', username).then(function(user) {
       // Check if user doesn't exist
-      if(user.length < 1) {
-        return done(null, false, { message: 'Incorrect username.' });
+      if (user.length < 1) {
+        return done(null, false, {
+          message: 'Incorrect username.'
+        });
       }
       // Check if password is invalid
-      if(!bcrypt.compareSync(password, user[0].password)) {
-        return done(null, false, { message: 'Incorrect password.' });
+      if (!bcrypt.compareSync(password, user[0].password)) {
+        return done(null, false, {
+          message: 'Incorrect password.'
+        });
       }
       // If all passes tests, then let the user in
       return done(null, user[0]);
@@ -52,16 +61,19 @@ passport.use(new LocalStrategy(
   }
 ));
 
+//When the user hits the login button, if they are authenticated, direct them to their user page, if not, redirect back to the home directory
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/user',
   failureRedirect: '/'
 }));
 
+//Serializes user data
 passport.serializeUser(function(user, done) {
   console.log('serializeUser', user);
   done(null, user);
 });
 
+//Deserializes user data
 passport.deserializeUser(function(id, done) {
   console.log('deserializeUser', id);
   done(null, id);
